bfb84d3f1e32d8f87e15f876f677ef2d8c3e2bb9
mahendra-singh132 <sisodiyamahii8092@gmail.com>
2026-09-26 19:24:40 +0530

    Debug Fix some name extention with .js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,8 +1,8 @@
 
-import { User } from "../models/user.model";
-import { ApiError } from "../utils/apiErrors";
-import { asyncHandler } from "../utils/asyncHandler";
-import { jwt } from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/apiErrors.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import  jwt  from "jsonwebtoken";
 
 
 export const verifyJWT = asyncHandler(async (req,res,next)=>{
